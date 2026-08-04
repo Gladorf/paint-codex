ffcbf8012eaea9e14ccf7929df5c267155d6ca24
Alexander Soumal <210098443+Gladorf@users.noreply.github.com>
2026-08-04 21:52:09 +0200

    Affiche les figurines depuis l'API

--- paint_codex/settings.py
+++ paint_codex/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'figurines',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+]
