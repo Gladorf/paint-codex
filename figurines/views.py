ab9dc760fa57bcba126d67d5ffe2d2f10337ca69
Alexander Soumal <210098443+Gladorf@users.noreply.github.com>
2026-08-28 14:55:57 +0200

    protege ecriture

--- figurines/views.py
+++ figurines/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import Figurine, Step
 from .serializers import FigurineSerializer, StepSerializer
@@ -7,8 +7,10 @@ from .serializers import FigurineSerializer, StepSerializer
 class FigurineViewSet(viewsets.ModelViewSet):
     queryset = Figurine.objects.all()
     serializer_class = FigurineSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class StepViewSet(viewsets.ModelViewSet):
     queryset = Step.objects.all()
     serializer_class = StepSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
